47f5067adfd04ae9a29a2e7765ed9e21595cf285
risりす <surisuririsu@gmail.com>
2024-07-10 18:54:57 -0400

    Wrap in suspense for search params

--- app/page.js
+++ app/page.js
@@ -1,4 +1,5 @@
 "use client";
+import { Suspense } from "react";
 import PlanSelect from "@/components/PlanSelect";
 import IdolSelect from "@/components/IdolSelect";
 import Configurator from "@/components/Configurator";
@@ -7,17 +8,19 @@ import styles from "./page.module.scss";
 
 export default function Home() {
   return (
-    <IdolContextProvider>
-      <main className={styles.main}>
-        <div className={styles.content}>
-          <div className={styles.header}>
-            <PlanSelect />
-            <IdolSelect />
+    <Suspense>
+      <IdolContextProvider>
+        <main className={styles.main}>
+          <div className={styles.content}>
+            <div className={styles.header}>
+              <PlanSelect />
+              <IdolSelect />
+            </div>
+            <hr />
+            <Configurator />
           </div>
-          <hr />
-          <Configurator />
-        </div>
-      </main>
-    </IdolContextProvider>
+        </main>
+      </IdolContextProvider>
+    </Suspense>
   );
 }
