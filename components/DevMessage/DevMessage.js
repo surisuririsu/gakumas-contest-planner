b994978bbb4355956169c91aa8b42cb79a8b96e1
risりす <surisuririsu@gmail.com>
2024-07-30 15:33:24 -0400

    Fix styling

--- components/DevMessage/DevMessage.js
+++ components/DevMessage/DevMessage.js
@@ -1,13 +1,7 @@
-"use client";
 import styles from "./DevMessage.module.scss";
 
 export default function DevMessage() {
-  const isMobile =
-    /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
-      navigator.userAgent
-    );
-
-  return isMobile ? null : (
+  return (
     <div className={styles.message}>
       <a href="https://gktools.ris.moe">
         Try out Gakumas Tools (In development)
